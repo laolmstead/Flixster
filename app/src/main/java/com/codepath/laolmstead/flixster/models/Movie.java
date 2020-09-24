e72c7c6a5abe95e306e5f991188c39d68e169268
Lindsey Olmstead <laolmstead19@gmail.com>
2020-09-24 00:44:09 -0400

    Initial Flixster commit

---
+++ app/src/main/java/com/codepath/laolmstead/flixster/models/Movie.java
@@ -0,0 +1,44 @@
+package com.codepath.laolmstead.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+    String posterPath;
+    String title;
+    String overview;
+    String posterSize;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
